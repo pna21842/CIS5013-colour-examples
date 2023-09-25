7d8f903a6b4b9c25ab78408ab0db4ed3f6bc8d7f
pna16384 <pna16384@gmail.com>
2023-09-25 19:35:00 +0100

    Model solution for workshop 1;  added rudimentary movement to demo keyboard handling

--- glDemo/main.cpp
+++ glDemo/main.cpp
@@ -8,6 +8,22 @@
 const unsigned int initWidth = 512;
 const unsigned int initHeight = 512;
 
+// Example variables to change when doing keyboard input
+
+// the position of our object
+float x = 0.0f;
+float y = 0.0f;
+
+// amount we change x, y when keys are pressed
+const float movementSpeed = 0.005f;
+
+// flags to track when a movement key is pressed
+bool wPressed = false;
+bool sPressed = false;
+bool aPressed = false;
+bool dPressed = false;
+
+
 // Function prototypes
 void renderScene();
 void resizeWindow(GLFWwindow* window, int width, int height);
@@ -58,6 +74,8 @@ int main() {
 	// Initialise scene - geometry and shaders etc
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f); // setup background colour to be black
 
+	// *** setup viewplane to the appropriate size
+	gluOrtho2D(-8.0f, 8.0f, -8.0f, 8.0f);
 
 	//
 	// 2. Main loop
@@ -90,6 +108,43 @@ void renderScene()
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	// Render objects here...
+
+	// *** triangle of the original size
+	/*glBegin(GL_TRIANGLES);
+
+	glVertex2f(-0.5f, -0.5f);
+	glVertex2f(0.0f, 0.5f);
+	glVertex2f(0.5f, -0.5f);
+
+	glEnd();*/
+
+	// *** square from 2 triangles
+	/*glBegin(GL_TRIANGLES);
+
+	glVertex2f(-0.5f, -0.5f);
+	glVertex2f(-0.5f, 0.5f);
+	glVertex2f(0.5f, 0.5f);
+
+	glVertex2f(0.5f, 0.5f);
+	glVertex2f(0.5f, -0.5f);
+	glVertex2f(-0.5f, -0.5f);
+	
+	glEnd();*/
+
+
+	// *** an extra - adding a moveable triangle
+	// Note: This is using an offset (x, y) to represent where the object is in the scene.
+	// There are 2 fundamental things to address here: 
+	// 1) This is a basic example of translation - when we introduce transformations, we'll look at doing this more effectively with matrices
+	// 2) Anything we animate needs to be based on time elapsed.  This code is not (it's only a basic intro :)  We'll look at timing for animation later!
+	glBegin(GL_TRIANGLES);
+
+	glVertex2f(x + -0.5f, y + -0.5f);
+	glVertex2f(x + 0.0f, y + 0.5f);
+	glVertex2f(x + 0.5f, y + -0.5f);
+
+	glEnd();
+
 }
 
 
@@ -112,6 +167,22 @@ void keyboardHandler(GLFWwindow* window, int key, int scancode, int action, int 
 				glfwSetWindowShouldClose(window, true);
 				break;
 
+			case GLFW_KEY_W:
+				wPressed = true;
+				break;
+
+			case GLFW_KEY_S:
+				sPressed = true;
+				break;
+
+			case GLFW_KEY_A:
+				aPressed = true;
+				break;
+
+			case GLFW_KEY_D:
+				dPressed = true;
+				break;
+
 			default:
 			{
 			}
@@ -119,11 +190,51 @@ void keyboardHandler(GLFWwindow* window, int key, int scancode, int action, int 
 	}
 	else if (action == GLFW_RELEASE) {
 		// handle key release events
+
+		// check which key was released...
+		switch (key)
+		{
+			case GLFW_KEY_W:
+				wPressed = false;
+				break;
+
+			case GLFW_KEY_S:
+				sPressed = false;
+				break;
+
+			case GLFW_KEY_A:
+				aPressed = false;
+				break;
+
+			case GLFW_KEY_D:
+				dPressed = false;
+				break;
+
+			default:
+				{
+				}
+		}
 	}
 }
 
 
 // Function called to animate elements in the scene
 void updateScene() {
+
+	if (wPressed) {
+		y += movementSpeed;
+	}
+
+	if (sPressed) {
+		y -= movementSpeed;
+	}
+
+	if (aPressed) {
+		x -= movementSpeed;
+	}
+
+	if (dPressed) {
+		x += movementSpeed;
+	}
 }
 
